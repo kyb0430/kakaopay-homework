8e34c1cd8fa47ae714c999d24462e4e3007c6b4b
김용범 <dkwlfkwl@adnstyle.com>
2020-01-17 21:29:39 +0900

    homework1

--- dist/js/homework-1/common.js
+++ dist/js/homework-1/common.js
@@ -5,8 +5,23 @@ function scrollCheck() {
 
   $('.scroll-check').each(function() {
     var $this = $(this);
-    var ratio = 1 - (Number($this.data('ratio')) || 80) / 100;
+    var ratio = 1 - (Number($this.data('ratio')) || 70) / 100;
     var isViewportIn = scrollTop > ($this.offset().top - winHeight) + (winHeight * ratio);
+
     $this.toggleClass('in', isViewportIn);
   });
 }
+
+$(function() {
+  // page init
+  $('body').addClass('init');
+
+  // scroll check
+  (function() {
+    var $window = $(window);
+
+    $('.scroll-check').addClass('ready');
+    $window.on('load resize scroll', scrollCheck);
+    scrollCheck();
+  })();
+});
--- dist/js/homework-2/common.js
+++ dist/js/homework-2/common.js
@@ -10,3 +10,17 @@ function scrollCheck() {
     $this.toggleClass('in', isViewportIn);
   });
 }
+
+$(function() {
+  // page init
+  $('body').addClass('init');
+
+  // scroll check
+  (function() {
+    var $window = $(window);
+
+    $('.scroll-check').addClass('ready');
+    $window.on('load resize scroll', scrollCheck);
+    scrollCheck();
+  })();
+});
