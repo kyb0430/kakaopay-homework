b7e6ade6cb9cd52617e186697b18315c25e9874a
unknown <dkwlfkwl@adnstyle.com>
2020-01-19 04:52:29 +0900

    promotion-1 작업

--- dist/js/homework-1/common.js
+++ dist/js/homework-1/common.js
@@ -5,16 +5,32 @@ function scrollCheck() {
 
   $('.scroll-check').each(function() {
     var $this = $(this);
-    var ratio = 1 - (Number($this.data('ratio')) || 70) / 100;
+    var ratio = 1 - (Number($this.data('ratio')) || 85) / 100;
     var isViewportIn = scrollTop > ($this.offset().top - winHeight) + (winHeight * ratio);
 
     $this.toggleClass('in', isViewportIn);
   });
 }
 
+function scrollMove() {
+  var $window = $(window);
+  var winHeight = $window.height();
+  var scrollTop = $window.scrollTop();
+  var ratio1 = -(winHeight * 0.1282 / winHeight * scrollTop);
+  var ratio2 = -(winHeight * 0.1602 / winHeight * scrollTop);
+  
+  $('.scroll-move-1').css('margin-top', ratio1);
+  $('.scroll-move-2').css('margin-top', ratio2);
+}
+
+
 $(function() {
   // page init
-  $('body').addClass('init');
+  
+  window.setTimeout(function() {
+    $('.page-loading').hide();
+    $('body').addClass('init');
+  }, 1500);
 
   // scroll check
   (function() {
@@ -22,6 +38,8 @@ $(function() {
 
     $('.scroll-check').addClass('ready');
     $window.on('load resize scroll', scrollCheck);
+    $window.on('load resize scroll', scrollMove);
     scrollCheck();
+    scrollMove();
   })();
 });
